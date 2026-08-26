1446de99d3c0cee904b5e2cbf2b62c1fcf157d4d
Pedro Riva <pedro.riva@gonsys.comn>
2026-08-26 13:26:24 -0300

    Add interactive instructions (how to close input on Windows/Linux)

--- bash2cmd.cs
+++ bash2cmd.cs
@@ -1,6 +1,14 @@
 using System;
 using System.Text;
 
+// Show instructions if running interactively (not piped)
+if (Console.IsInputRedirected == false)
+{
+    Console.Error.WriteLine("bash2cmd - Converts multiline bash commands to Windows CMD format.");
+    Console.Error.WriteLine("Paste your bash command, then press Ctrl+Z (Windows) or Ctrl+D (Linux) to convert.");
+    Console.Error.WriteLine();
+}
+
 // Read all input from stdin
 var input = Console.In.ReadToEnd();
 var lines = input.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
